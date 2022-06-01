6260922eda51eb546a3d9b68be0a384db877a44d
Jim Cronin <jmcronin@mitre.org>
2022-06-01 14:29:05 -0400

    Added retrievedAt column for STEVE.

---
+++ messaging/Migrations/20220601120000_Add-STEVE-RetrievedAt-Column.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace messaging.Migrations
+{
+    /// <summary>
+    /// Enable tracking retrieval of messages by STEVE separately from jurisdictions.
+    /// </summary>
+    public partial class AddSTEVERetrievedAtColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "STEVE_RetrievedAt",
+                table: "OutgoingMessageItems",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "STEVE_RetrievedAt",
+                table: "OutgoingMessageItems");
+        }
+    }
+}
