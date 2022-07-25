938b1471ea5ec0f94ddc77cd6bc45fd6aef6cbcc
Jim Cronin <jmcronin@mitre.org>
2022-07-25 07:54:01 -0400

    NVSS-287: Limit to page size before executing query.

--- messaging/Controllers/BundlesController.cs
+++ messaging/Controllers/BundlesController.cs
@@ -72,11 +72,11 @@ namespace messaging.Controllers
                 }
 
                 // Convert to list to execute the query, capture the result for re-use
-                List<OutgoingMessageItem> outgoingMessages = outgoingMessagesQuery.ToList();
+                List<OutgoingMessageItem> outgoingMessages = outgoingMessagesQuery.Take(_count).ToList();
 
                 // This uses the general FHIR parser and then sees if the json is a Bundle of BaseMessage Type
                 // this will improve performance and prevent vague failures on the server, clients will be responsible for identifying incorrect messages
-                IEnumerable<System.Threading.Tasks.Task<VRDR.BaseMessage>> messageTasks = outgoingMessages.Take(_count).Select(message => System.Threading.Tasks.Task.Run(() => BaseMessage.ParseGenericMessage(message.Message, true)));
+                IEnumerable<System.Threading.Tasks.Task<VRDR.BaseMessage>> messageTasks = outgoingMessages.Select(message => System.Threading.Tasks.Task.Run(() => BaseMessage.ParseGenericMessage(message.Message, true)));
 
                 // create bundle to hold the response
                 Bundle responseBundle = new Bundle();
