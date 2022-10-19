4fa720e4092b053fb11c41ab5e21e7a90eee0656
Robi Scalfani <rscalfani@mitre.org>
2022-10-19 11:22:44 -0400

    Setup Github Pages

--- messaging/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class AppSettings
-{
-    public Boolean AckAndIJEConversion { get; set; } = false;
-    public string SAMS {get; set;}
-    public string STEVE {get; set;}
-    public int PageCount {get; set;}
-}
--- messaging/NVSSAPI/input/pagecontent/index.md
+++
@@ -1,3 +0,0 @@
-# NVSSAPI
-
-Feel free to modify this index page with your own awesome content!
