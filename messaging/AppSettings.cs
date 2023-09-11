bf7cf56cde263372ddc4878808f63ab8793a61c9
MacAdam <smacadam@mitre.org>
2023-09-11 15:58:21 -0400

    Add auth settings

--- messaging/AppSettings.cs
+++ messaging/AppSettings.cs
@@ -6,4 +6,11 @@ public class AppSettings
     public string SAMS {get; set;}
     public string STEVE {get; set;}
     public int PageCount {get; set;}
+    public string ClientId {get; set;}
+    public string ClientSecret {get; set;}
+    public string AuthEndpoint {get; set;}
+    public string TokenEndpoint {get; set;}
+    public string UserInfo {get; set;}
+    public string CallbackPath {get; set;}
+    
 }
--- messaging/Startup.cs
+++ messaging/Startup.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,7 +13,11 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.IO;
 using System;
-
+using System.Text.Json;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace messaging
 {
@@ -33,6 +39,35 @@ namespace messaging
             services.AddDbContext<ApplicationDbContext>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("NVSSMessagingDatabase"))
             );
+            var settings = Configuration.GetSection("AppSettings");
+            // remove the cookies?
+            services.AddAuthentication("cookie")
+            .AddCookie("cookie")
+            .AddOAuth("github", o =>
+            {
+                o.SignInScheme = "cookie";
+                o.ClientId = settings["ClientId"];
+                o.ClientSecret = settings["ClientSecret"];
+                o.AuthorizationEndpoint = settings["AuthEndpoint"];
+                o.TokenEndpoint = settings["TokenEndpoint"];
+                o.UserInformationEndpoint = settings["UserInfo"];
+                o.SaveTokens = true;
+                o.CallbackPath = settings["CallbackPath"];
+
+                o.ClaimActions.MapJsonKey("sub","id");
+                o.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+
+                o.Events.OnCreatingTicket = async ctx =>
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+                    using var result = await ctx.Backchannel.SendAsync(request);
+                    var user = await result.Content.ReadFromJsonAsync<JsonElement>(); 
+                    ctx.RunClaimActions(user);
+                };
+
+            });
+
             services.AddControllers().AddNewtonsoftJson(
                 options => options.SerializerSettings.Converters.Add(new BundleConverter())
             );
@@ -48,11 +83,13 @@ namespace messaging
                 {
                     options.SuppressMapClientErrors = true;
                 });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+
             app.UseHttpLogging();
             app.UseHttpsRedirection();
             if (env.IsDevelopment())
@@ -69,17 +106,26 @@ namespace messaging
             }
             app.UseMiniProfiler();
             app.UseRouting();
+
+            // auth must be added between app.UseRouting and app.UseEndpoints
+            app.UseAuthentication();
             app.UseAuthorization();
-            if (env.IsProduction())
-            {
-                // authentication callback for production
-                app.UseMiddleware<ExtractCustomHeaderMiddleware>();
-            }
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                // the default redirect when the request is unauthorized is /Account/Login
+                endpoints.MapGet("/Account/Login", (HttpContext ctx) => 
+                {   return Results.Challenge(
+                    new AuthenticationProperties()
+                    {
+                        RedirectUri = "https://localhost:5001/TT/metadata"
+                    },
+                    authenticationSchemes: new List<string>() { "github" }
+                    ); 
+                });
+                // require authorization for all endpoints
+                endpoints.MapControllers().RequireAuthorization();
             });
         }
     }
-
 }
