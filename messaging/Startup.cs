548372830bbfa833e8a3193ad3a7529fa75ab14e
MacAdam <smacadam@mitre.org>
2024-10-10 17:16:20 -0400

    add max payload size to app settings

--- messaging/Startup.cs
+++ messaging/Startup.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.IO;
 using System;
+using Microsoft.AspNetCore.Http.Features;
 
 
 namespace messaging
@@ -28,6 +29,7 @@ namespace messaging
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
             services.AddMemoryCache();
             services.AddMiniProfiler(options => options.RouteBasePath = "/profiler").AddEntityFramework();
             services.AddDbContext<ApplicationDbContext>(opt =>
@@ -67,6 +69,7 @@ namespace messaging
                 context.Response.Headers.Add("X-XSS-Protection", "1;mode=block");
                 context.Response.Headers.Add("Cache-Control", "no-store");
                 context.Response.Headers.Add("Content-Security-Policy", "default-src");
+                context.Features.Get<IHttpMaxRequestBodySizeFeature>().MaxRequestBodySize = Int32.Parse(Configuration.GetSection("AppSettings").GetSection("MaxPayloadSize").Value);
                 await next.Invoke();
             });
             if (env.IsDevelopment())
