f9228eeaf326860b7b79cceaca28c508cb68914d
Pete K <krautscheid@mitre.org>
2024-10-15 16:35:13 -0400

    Add three indexes to improve status API performance

---
+++ messaging/Migrations/20241015200019_AddIndexesToIncomingMessageItems.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace messaging.Migrations
+{
+    public partial class AddIndexesToIncomingMessageItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_IncomingMessageItems_CreatedDate",
+                table: "IncomingMessageItems",
+                column: "CreatedDate");
+            migrationBuilder.CreateIndex(
+                name: "IX_IncomingMessageItems_UpdatedDate",
+                table: "IncomingMessageItems",
+                column: "UpdatedDate");
+            migrationBuilder.CreateIndex(
+                name: "IX_IncomingMessageItems_ProcessedStatus",
+                table: "IncomingMessageItems",
+                column: "ProcessedStatus");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_IncomingMessageItems_CreatedDate",
+                table: "IncomingMessageItems");
+            migrationBuilder.DropIndex(
+                name: "IX_IncomingMessageItems_UpdatedDate",
+                table: "IncomingMessageItems");
+            migrationBuilder.DropIndex(
+                name: "IX_IncomingMessageItems_ProcessedStatus",
+                table: "IncomingMessageItems");
+        }
+    }
+}
