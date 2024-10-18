99cb08aae840424b810cdb144256d6b5830f821f
Pete K <krautscheid@mitre.org>
2024-10-18 11:04:03 -0400

    Status UI layout improvements

--- status_ui/src/main.jsx
+++ status_ui/src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
