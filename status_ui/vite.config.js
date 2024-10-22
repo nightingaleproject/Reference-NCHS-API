6a776485c5cd64b5412db2286226d4ba5b5d42fa
Pete K <krautscheid@mitre.org>
2024-10-22 18:15:19 -0400

    Set up API to serve StatusUI files as appropriate

--- status_ui/vite.config.js
+++ status_ui/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '',
+  build: {
+    outDir: '../messaging/StatusUI',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/status': {
