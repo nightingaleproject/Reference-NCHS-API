09b2286a00b6c168953771390a68daa637a1ef03
RobertScalfani <rscalfani@mitre.org>
2025-03-27 15:45:18 -0400

    added igversion migration

---
+++ messaging/Migrations/20250327181615_IGVersion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace messaging.Migrations
+{
+    public partial class IGVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IGVersion",
+                table: "OutgoingMessageItems",
+                type: "CHAR(5)",
+                maxLength: 5,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IGVersion",
+                table: "IncomingMessageItems",
+                type: "CHAR(5)",
+                maxLength: 5,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IGVersion",
+                table: "OutgoingMessageItems");
+
+            migrationBuilder.DropColumn(
+                name: "IGVersion",
+                table: "IncomingMessageItems");
+        }
+    }
+}
