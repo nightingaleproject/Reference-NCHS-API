49c7148a809df576498c2d575ab2f12be1c9e639
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-04-30 15:22:57 -0400

    add ApplicationDbContext to builder services

--- status_api/Program.cs
+++ status_api/Program.cs
@@ -1,6 +1,14 @@
+using messaging.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString =
+    builder.Configuration.GetConnectionString("NVSSMessagingDatabase")
+        ?? throw new InvalidOperationException("Connection string 'NVSSMessagingDatabase' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
