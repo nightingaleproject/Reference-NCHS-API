b3a31713e2cf98e562a4e2bc42630db0eaed4b5a
Sarah M <smacadam@mitre.org>
2025-05-08 22:15:34 -0400

    fix stored procedures with params

---
+++ messaging/Migrations/20250506203854_UpdateRetrievedAtField.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace messaging.Migrations
+{
+    public partial class UpdateRetrievedAtField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var createProcedure = "CREATE PROCEDURE [dbo].[UpdateOutgoingMessagesRetrievedAt] @Id int, @RetrievedAt datetime AS BEGIN SET NOCOUNT ON UPDATE OutgoingMessageItems SET RetrievedAt = @RetrievedAt WHERE Id = @Id END";
+            migrationBuilder.Sql(createProcedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var dropProcedure = "DROP PROCEDURE [dbo].[UpdateOutgoingMessagesRetrievedAt]";
+            migrationBuilder.Sql(dropProcedure);
+        }
+    }
+}
