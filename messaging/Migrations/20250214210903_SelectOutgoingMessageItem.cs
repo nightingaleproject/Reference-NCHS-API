eb7389f60dff53278fce23559ec167474fbebfeb
Sarah M <smacadam@mitre.org>
2025-05-08 22:15:34 -0400

    fix stored procedures with params

--- messaging/Migrations/20250214210903_SelectOutgoingMessageItem.cs
+++ messaging/Migrations/20250214210903_SelectOutgoingMessageItem.cs
@@ -8,13 +8,13 @@ namespace messaging.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var createProcedure = "CREATE PROCEDURE [dbo].[SelectNewOutgoingMessageItemsWithParams] @JurisdictionId char(2), @EventYear bigint = NULL, @CertificateNumber char(6) = NULL, @EventType char(3) = NULL AS BEGIN SET NOCOUNT ON; SELECT * FROM OutgoingMessageItems WHERE JurisdictionId = @JurisdictionId AND EventYear = (CASE WHEN @EventYear IS NOT NULL THEN @EventYear ELSE EventYear END) AND CertificateNumber = (CASE WHEN @CertificateNumber IS NOT NULL THEN @CertificateNumber ELSE CertificateNumber END) AND EventType = (CASE WHEN @EventType IS NOT NULL THEN @EventType ELSE EventType END) END";
+            var createProcedure = "CREATE PROCEDURE [dbo].[SelectNewOutgoingMessageOrdered] @JurisdictionId char(2), @EventType char(3) = NULL, @Count int AS BEGIN SET NOCOUNT ON; SELECT * FROM OutgoingMessageItems WHERE JurisdictionId = @JurisdictionId AND EventType = (CASE WHEN @EventType IS NOT NULL THEN @EventType ELSE EventType END) AND RetrievedAt IS NULL ORDER BY CreatedDate OFFSET 0 ROWS FETCH NEXT @Count ROWS ONLY END;";
             migrationBuilder.Sql(createProcedure);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            var dropProcedure = "DROP PROCEDURE [dbo].[SelectNewOutgoingMessageItemsWithParams]";
+            var dropProcedure = "DROP PROCEDURE [dbo].[SelectNewOutgoingMessageOrdered]";
             migrationBuilder.Sql(dropProcedure);
         }
     }
---
+++ messaging/Migrations/20250506202544_CountOutgoingMessageItems.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace messaging.Migrations
+{
+    public partial class CountOutgoingMessageItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var createProcedure = "CREATE PROCEDURE [dbo].[CountNewOutgoingMessages] @JurisdictionId char(2), @EventType char(3) = NULL AS BEGIN SET NOCOUNT ON; SELECT CAST(COUNT(*) AS bigint) AS Id, 'NA' AS CertificateNumber, GETDATE() AS CreatedDate, GETDATE() AS UpdatedDate, 'NA' AS EventType, 'NA' AS Message, 'NA' AS MessageType, 'NA' AS MessageId, 'NA' AS JurisdictionId, CAST(1 AS bigint) AS EventYear, NULL AS RetrievedAt FROM OutgoingMessageItems WHERE JurisdictionId = @JurisdictionId AND EventType = (CASE WHEN @EventType IS NOT NULL THEN @EventType ELSE EventType END) AND RetrievedAt IS NULL END";
+            migrationBuilder.Sql(createProcedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var dropProcedure = "DROP PROCEDURE [dbo].[CountNewOutgoingMessages]";
+            migrationBuilder.Sql(dropProcedure);
+        }
+    }
+}
