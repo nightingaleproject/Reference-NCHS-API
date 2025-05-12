39bdf7c1b3a365ceca4aabc19479db3ebbf008ff
Sarah M <smacadam@mitre.org>
2025-05-12 12:42:10 -0400

    update appsettings to enable birth and fetal death separately, set up auto acks for fd

--- messaging/AppSettings.cs
+++ messaging/AppSettings.cs
@@ -3,7 +3,8 @@ using System;
 public class AppSettings
 {
     public Boolean AckAndIJEConversion { get; set; } = false;
-    public Boolean BFDREnabled { get; set; } = false;
+    public Boolean BirthEnabled { get; set; } = false;
+    public Boolean FetalDeathEnabled { get; set; } = false;
     public string SAMS {get; set;}
     public string STEVE {get; set;}
     public int PageCount {get; set;}
