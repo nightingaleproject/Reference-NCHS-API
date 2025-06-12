9eab534913a4fb2757d6739c85898fb1371cf3c5
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-06-12 14:51:35 -0400

    remove MaxPayloadSize from status_api

--- status_api/AppSettings.cs
+++ status_api/AppSettings.cs
@@ -5,6 +5,5 @@ namespace status_api
     public class AppSettings
     {
         public string Environment {get; set;} = String.Empty;
-        public int MaxPayloadSize {get; set;} = 10000000;
     }
 }
