0d3405428e68d8c690f78bb9a21dd97856888f05
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-07-21 15:11:58 -0400

    Status UI Release v1.0.1 (#128)

--- status_ui/src/Version.jsx
+++ status_ui/src/Version.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography, Box } from '@mui/material';
 
 export const Version = () => {
-  const STATUS_UI_VERSION = '1.0.0';
+  const STATUS_UI_VERSION = '1.0.1';
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
       <Typography variant="h6" sx={{ my: 2, fontSize: '1.0rem' }}>
